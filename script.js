3864ddfc55e145e8d74f6323c40a519d751c5340
Blake <fosterbhilleshiem@gmail.com>
2022-07-12 21:30:47 -0600

    got weighted list working

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let listOfPeople = [];
-
 let apiGet = {
   message: "Users found",
   users: [
@@ -65,9 +63,10 @@ let apiGet = {
     },
   ],
 };
-
+// --------- api portion ^^^
 // console.log(apiGet.users[0].first_name);
 
+let listOfPeople = [];
 for (let obj of apiGet.users) {
   let name =
     obj.first_name[0].toUpperCase() +
@@ -78,40 +77,31 @@ for (let obj of apiGet.users) {
   listOfPeople.push(name);
 }
 
-function increaseWeight(user) {
-  listOfPeople.push(user);
-}
+// ----- list of People ^^^
 
-function getIndex() {
-  return new Promise((res, rej) => {
-    res(Math.floor(Math.random() * (listOfPeople.length - 1)));
-  });
-}
+// function increaseWeight(user) {
+//   listOfPeople.push(user);
+// }
 
-async function selectLuckyOne() {
-  // let index = Math.floor(Math.random() * (13 - 0) + 0);
-  result = await getIndex();
-  // console.log(result);
-  // console.log(listOfPeople[result]);
-  listOfPeople.splice(result, 1);
-  // console.log(listOfPeople);
-}
+// function getIndex() {
+//   return new Promise((res, rej) => {
+//     res(Math.floor(Math.random() * (listOfPeople.length - 1)));
+//   });
+// }
 
-increaseWeight(listOfPeople[2]);
-increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// increaseWeight(listOfPeople[2]);
-// console.log(listOfPeople);
+// let listOfWeights = generateListOfWeights(listOfElPeopleWeights);
+// console.log(generateWeightedList(listOfPeople, listOfWeights));
 
-// selectLuckyOne();
+// async function selectLuckyOne() {
+//   let result = await getIndex();
 
-// console.log(Math.floor(Math.random() * (13 - 0) + 0));
+//   let person = listOfPeople[result];
+
+//   // listOfPeople.splice(result, 1);
+
+//   return person;
+// }
+// -------------------------- was working ^^^
 const rowsOfPeople = document.getElementById("list-of-people");
 
 function createParticipants(name) {
@@ -120,7 +110,7 @@ function createParticipants(name) {
   listItemEl.id = name;
 
   const listItemName = document.createElement("div");
-  const listItemNameText = document.createTextNode(name);
+  const listItemNameText = document.createTextNode(`${name} : `);
   listItemName.appendChild(listItemNameText);
   listItemName.className = "name-wrapper";
 
@@ -128,49 +118,143 @@ function createParticipants(name) {
   valueTracker.className = "value-tracker-wrapper";
 
   const upWeightBtn = document.createElement("button");
-  const upWeightBtnIcon = document.createTextNode(">");
+  const upWeightBtnIcon = document.createTextNode("+");
   upWeightBtn.appendChild(upWeightBtnIcon);
   upWeightBtn.className = "increaseBtn";
 
   const weightValue = document.createElement("div");
-  // weightValue.innerText = "0";
-  const weightValueText = document.createTextNode("0");
+  const weightValueText = document.createTextNode("1");
   weightValue.appendChild(weightValueText);
+  weightValue.className = "weight-value-text";
 
   const lowerWeightBtn = document.createElement("button");
-  const lowerWeightBtnIcon = document.createTextNode("<");
+  const lowerWeightBtnIcon = document.createTextNode("-");
   lowerWeightBtn.appendChild(lowerWeightBtnIcon);
   lowerWeightBtn.className = "decreaseBtn";
 
   valueTracker.appendChild(lowerWeightBtn);
-  valueTracker.appendChild(weightValue);
   valueTracker.appendChild(upWeightBtn);
 
   listItemEl.appendChild(listItemName);
+  listItemName.appendChild(weightValue);
   listItemEl.appendChild(valueTracker);
 
   rowsOfPeople.appendChild(listItemEl);
 
   upWeightBtn.addEventListener("click", () => {
     weightValue.innerText = Number(weightValue.innerText) + 1;
-    // console.log(listItemName.innerText);
-    // ---------
-    // listOfPeople.push(listItemName.innerText);
-    // console.log(listOfPeople);
+    lowerWeightBtn.disabled = false;
   });
 
   lowerWeightBtn.addEventListener("click", () => {
     weightValue.innerText = Number(weightValue.innerText) - 1;
-    // console.log(listItemName.innerText);
-    // ------
-    // listOfPeople.push(listItemName.innerText);
-    // console.log(listOfPeople);
+    if (Number(weightValue.innerText) == 0) {
+      lowerWeightBtn.disabled = true;
+    }
   });
 }
 
 for (let name of listOfPeople) {
   createParticipants(name);
 }
+
+// -------- fills Dom with names ^^^
+
+const listOfElPeopleWeights =
+  document.getElementsByClassName("weight-value-text");
+
+const generateListOfWeights = (listOfEl) => {
+  let listOfWeights = [];
+  for (el of listOfEl) {
+    listOfWeights.push(el.innerText);
+  }
+  return listOfWeights;
+};
+
+function generateWeightedList(listOfNames, listOfValues) {
+  function customRange(num) {
+    let timesToRun = [];
+    while (num != 0) {
+      timesToRun.push("");
+      num--;
+    }
+    return timesToRun;
+  }
+  let weightedListOfPeople = [];
+  indexCount = 0;
+  for (let name of listOfNames) {
+    for (let i in customRange(listOfValues[indexCount])) {
+      weightedListOfPeople.push(name);
+    }
+    indexCount++;
+  }
+  return weightedListOfPeople;
+}
+
+let lstWghts = generateListOfWeights(listOfElPeopleWeights);
+console.log(lstWghts);
+let wghtLstNames = generateWeightedList(listOfPeople, lstWghts);
+console.log(wghtLstNames);
+
+// --------------------- generates weighted list ^^^
+
+async function selectLuckyOne(weightedList) {
+  function getIndex() {
+    return new Promise((res, rej) => {
+      res(Math.floor(Math.random() * (weightedList.length - 1)));
+    });
+  }
+  let result = await getIndex();
+  let person = weightedList[result];
+  // listOfPeople.splice(result, 1);
+  return person;
+}
+
+let theOne = selectLuckyOne(wghtLstNames);
+theOne.then((response) => console.log(response));
+
+// console.log(theOne);
+// console.log(returnName());
+// ----------------------- gets name from list ^^^
+
+function increaseWeight(user) {
+  listOfPeople.push(user);
+}
+
+const divsOfPeople = document.getElementsByClassName("name-wrapper");
+const displayName = document.getElementById("name-spinner");
+const spinButton = document.getElementById("spin-button");
+
+spinButton.addEventListener("click", () => {
+  // async function generateDisplay() {
+  //   let person = await selectLuckyOne();
+  //   displayName.innerText = person;
+  //   displayName.innerText = person;
+  // }
+  // let person = generateDisplay();
+  let lstWghts = generateListOfWeights(listOfElPeopleWeights);
+  console.log(lstWghts);
+  let wghtLstNames = generateWeightedList(listOfPeople, lstWghts);
+  console.log(wghtLstNames);
+  console.log(wghtLstNames);
+  let theOne = selectLuckyOne(wghtLstNames);
+  theOne.then((response) => {
+    displayName.innerText = response;
+  });
+
+  // for (let div of divsOfPeople) {
+  //   div.style = "color: white;";
+  //   console.log(div.innerText);
+  //   if (div.innerText == `${person} : `) {
+  //     div.style = "color:red;";
+  //   }
+  // }
+});
+
+// let listOfValues = generateListOfWeights(listOfElPeopleWeights);
+// weightedList = generateWeightedList(listOfPeople, listOfValues);
+// console.log(selectLuckyOne(weightedList));
+// *********************************************************
 
 // function createUser(text) {
 //   const completeBtn = document.createElement("button");
@@ -223,18 +307,137 @@ for (let name of listOfPeople) {
 
 // --------------------------------
 
-let multiplierRef = {
-  "Blake Hilleshiem": 1,
-  "Arlen Runolfuson": 1,
-  "Jacob Grimshaw": 1,
-};
+// const listOfElPeopleWeights =
+//   document.getElementsByClassName("weight-value-text");
 
-multiplierRef["Blake Hilleshiem"] = 2;
+// function customRange(num) {
+//   let timesToRun = [];
+//   while (num != 0) {
+//     timesToRun.push("");
+//     num--;
+//   }
+//   return timesToRun;
+// }
 
-let constructList = [];
-for (let key in multiplierRef) {
-  console.log(key);
-  console.log(multiplierRef[key]);
-}
+// const generateListOfWeights = (listOfEl) => {
+//   let listOfWeights = [];
+//   for (el of listOfEl) {
+//     listOfWeights.push(el.innerText);
+//   }
+//   return listOfWeights;
+// };
 
-console.log(constructList);
+// const generateWeightedList = (listOfNames, listOfValues) => {
+//   let weightedListOfPeople = [];
+//   indexCount = 0;
+//   for (let name of listOfNames) {
+//     for (let i in customRange(listOfValues[indexCount])) {
+//       weightedListOfPeople.push(name);
+//     }
+//     indexCount++;
+//   }
+//   return weightedListOfPeople;
+// };
+
+// let listOfWeights = generateListOfWeights(listOfElPeopleWeights);
+// console.log(generateWeightedList(listOfPeople, listOfWeights));
+
+// ---------------------^^^^
+
+// console.log(weightedListOfPeople);
+// console.log(listOfPeople);
+// generateWeightedList(listOfPeople, [0, 0, 1, 2, 0, 0, 3, 0, 4, 0, 5, 1]);
+// console.log(weightedListOfPeople);
+
+// ---------- working add function
+
+// function createParticipants(name) {
+//   const listItemEl = document.createElement("div");
+//   listItemEl.className = "user-container";
+//   listItemEl.id = name;
+
+//   const listItemName = document.createElement("div");
+//   const listItemNameText = document.createTextNode(name);
+//   listItemName.appendChild(listItemNameText);
+//   listItemName.className = "name-wrapper";
+
+//   const valueTracker = document.createElement("div");
+//   valueTracker.className = "value-tracker-wrapper";
+
+//   const upWeightBtn = document.createElement("button");
+//   const upWeightBtnIcon = document.createTextNode(">");
+//   upWeightBtn.appendChild(upWeightBtnIcon);
+//   upWeightBtn.className = "increaseBtn";
+
+//   const weightValue = document.createElement("div");
+//   // weightValue.innerText = "0";
+//   const weightValueText = document.createTextNode("0");
+//   weightValue.appendChild(weightValueText);
+
+//   const lowerWeightBtn = document.createElement("button");
+//   const lowerWeightBtnIcon = document.createTextNode("<");
+//   lowerWeightBtn.appendChild(lowerWeightBtnIcon);
+//   lowerWeightBtn.className = "decreaseBtn";
+
+//   valueTracker.appendChild(lowerWeightBtn);
+//   valueTracker.appendChild(weightValue);
+//   valueTracker.appendChild(upWeightBtn);
+
+//   listItemEl.appendChild(listItemName);
+//   listItemEl.appendChild(valueTracker);
+
+//   rowsOfPeople.appendChild(listItemEl);
+
+//   upWeightBtn.addEventListener("click", () => {
+//     weightValue.innerText = Number(weightValue.innerText) + 1;
+//     // console.log(listItemName.innerText);
+//     // ---------
+//     // listOfPeople.push(listItemName.innerText);
+//     // console.log(listOfPeople);
+//   });
+
+//   lowerWeightBtn.addEventListener("click", () => {
+//     weightValue.innerText = Number(weightValue.innerText) - 1;
+//     // console.log(listItemName.innerText);
+//     // ------
+//     // listOfPeople.push(listItemName.innerText);
+//     // console.log(listOfPeople);
+//   });
+// }
+
+// -----------------------------------
+
+// async function getSwapi() {
+//   const data = await fetch()
+//   console.log("1", data);
+//   console.log();
+//   console.log("2", data.result.properties.name);
+// }
+
+// -------
+// const swapiData = new Promise((resolve, reject) => {
+//   try {
+//     fetch("https://www.swapi.tech/api/people/1")
+//       .then((res) => resolve(res.json()))
+//       .catch((err) => new Error("Oops"));
+//   } catch (err) {
+//     reject(err);
+//   }
+// });
+
+// async function getSwapi() {
+//   const data = await swapiData;
+
+//   console.log("1", data);
+//   console.log();
+//   console.log("2", data.result.properties.name);
+// }
+// // async function getSwapi() {
+// //   const data = await fetch()
+// //   console.log("1", data);
+// //   console.log();
+// //   console.log("2", data.result.properties.name);
+// // }
+// // ^^ what you would do realistically
+
+// getSwapi();
